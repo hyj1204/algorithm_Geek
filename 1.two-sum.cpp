e34ccbe5adf72bb7fd9e0ed5d5cc08e3df186249
huangyijing <hyj891204@gmail.com>
2020-07-03 16:06:47 -0700

    hash map

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -26,46 +26,69 @@
 //     }
 // };
  
-//way 2:
+//way 2: double points
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         vector<int> result;
+//         vector<int> sorted_nums;
+
+//         //new sorted array
+//         sorted_nums = nums;
+//         sort(sorted_nums.begin(), sorted_nums.end());
+
+//         //get the 2 num we want from sorted array by two pointers technique
+//         int i = 0;
+//         int j = sorted_nums.size() - 1;
+//         while (i < j) {
+//             if (sorted_nums[i] + sorted_nums[j] > target)
+//                 j--;
+//             else if (sorted_nums[i] + sorted_nums[j] < target)
+//                 i++;
+//             else
+//             {
+//                 // find their indices in origin array
+//                 int find_i = 0;
+//                 int find_j = 0;
+//                 for (int z = 0; z < nums.size(); z++)
+//                 {
+//                     if (!find_i && nums[z] == sorted_nums[i]) {
+//                         result.push_back(z);
+//                         find_i = 1;
+//                     } else if (!find_j && nums[z] == sorted_nums[j]) {
+//                         result.push_back(z);
+//                         find_j = 1;
+//                     }
+//                     if (find_i && find_j)
+//                         return result;
+//                 } 
+//             }    
+//         }
+//         return result;
+//     }
+// };
+
+// way 3 : hash 
+// for each a : 
+//     check(target -a) exists in this array
+// 因为题目要求是假设只有一个solution,
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> result;
-        vector<int> sorted_nums;
+        unordered_map<int, int> check_map;
+        //新建一个check_map，key是arr里面的数，value是这个数的index
+        //然后对于arr中的每一个数，
+        for (int i = 0; i < nums.size(); i++) {
+            //对于arr中的每一个数
+            int check_num = target - nums[i];
 
-        //new sorted array
-        sorted_nums = nums;
-        sort(sorted_nums.begin(), sorted_nums.end());
-
-        //get the 2 num we want from sorted array by two pointers technique
-        int i = 0;
-        int j = sorted_nums.size() - 1;
-        while (i < j) {
-            if (sorted_nums[i] + sorted_nums[j] > target)
-                j--;
-            else if (sorted_nums[i] + sorted_nums[j] < target)
-                i++;
-            else
-            {
-                // find their indices in origin array
-                int find_i = 0;
-                int find_j = 0;
-                for (int z = 0; z < nums.size(); z++)
-                {
-                    if (!find_i && nums[z] == sorted_nums[i]) {
-                        result.push_back(z);
-                        find_i = 1;
-                    } else if (!find_j && nums[z] == sorted_nums[j]) {
-                        result.push_back(z);
-                        find_j = 1;
-                    }
-                    if (find_i && find_j)
-                        return result;
-                } 
-            }    
+            if (check_map.count(check_num)) //判断要找的差值是不是已经在map里了
+                return { check_map[check_num], i}; //如果是的话，就返回这个差值的index 和 当前这个数的index
+            check_map[nums[i]] = i; //key是arr里面的数，value是这个数的index
         }
-        return result;
+        return {};
     }
 };
+
 // @lc code=end
 
